b36851af86ef831b0823f59b8111892da65552cf
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2023-08-05 22:32:48 +0530

    lexer api just takes string

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -3,8 +3,11 @@ module Lexer (lexer) where
 import Lexer.Internal (lexLine)
 import Token (TokenInfo (..))
 
-lexer :: [String] -> [TokenInfo]
-lexer fileLines = zip [1 ..] (lexLine <$> fileLines) >>= addRowInfo
+lexer' :: [String] -> [TokenInfo]
+lexer' fileLines = zip [1 ..] (lexLine <$> fileLines) >>= addRowInfo
   where
     addRowInfo (row, tokens) = isomorph <$> fmap (row,) tokens
     isomorph (row, (token, col)) = TokenInfo {token = token, startRow = row, startCol = col}
+
+lexer :: String -> [TokenInfo]
+lexer = lexer' . lines
